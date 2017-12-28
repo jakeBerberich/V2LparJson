1e40e96209c4a136d6f11c3a0720d1cbd5752493
jake berberich <jake.berberich@me.com>
2017-12-28 09:29:04 -0500

    Initial Commit

---
+++ V2LparJson/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  V2LparJson
+//
+//  Created by Jake Berberich on 12/28/17.
+//  Copyright © 2017 Jake Berberich. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
